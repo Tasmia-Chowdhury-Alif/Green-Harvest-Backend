aabf78d8c7b91a370e5c97c3e65a98fe9cbf30d2
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2026-01-30 13:10:14 +0600

    fix: User Profile returned non-string error resolved

--- users/models.py
+++ users/models.py
@@ -33,4 +33,4 @@ class Profile(models.Model):
     postcode = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return self.user.get_full_name() or self.user.username
+        return f"Profile of {self.user.email}"
