2cf8416a58fad55c092a4e4a4bf40701c4128bae
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2026-01-30 13:07:05 +0600

    registered signals

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    
+    def ready(self):
+        # Import signals here — this is the standard & safe place
+        import core.signals
