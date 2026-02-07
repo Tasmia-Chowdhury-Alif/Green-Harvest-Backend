06ae57a581cdc47b19703bd1386140684babb5a2
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2026-02-07 23:58:33 +0600

    updated max page size limit for pagination

--- products/pagination.py
+++ products/pagination.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 class ProductListPagination(PageNumberPagination):
     page_size = 16
     page_size_query_param = "page_size"   # optional (allows frontend override)
-    # max_page_size = 30                     # safety limit
+    max_page_size = 70                     # safety limit
