8c92eadc0b87347ef55af214eab1113936d0f4a6
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2026-03-31 00:08:56 +0600

    updated rating to min_rating and added filter by in_stock

--- products/filters.py
+++ products/filters.py
@@ -1,12 +1,13 @@
 import django_filters
-from .models import Product, Category
+from .models import Product, Category, Tag
 
 class ProductFilter(django_filters.FilterSet):
     category = django_filters.CharFilter(method='filter_by_category')  # ?category=slug (includes subcatsagory)
     min_price = django_filters.NumberFilter(field_name='current_price', lookup_expr='gte')  # ?min_price=10
     max_price = django_filters.NumberFilter(field_name='current_price', lookup_expr='lte')  # ?max_price=50
-    rating = django_filters.NumberFilter(field_name='average_rating', lookup_expr='gte')  # ?rating=4 (min rating)
+    min_rating = django_filters.NumberFilter(field_name='average_rating', lookup_expr='gte')  # ?min_rating=4 (min rating)
     tags = django_filters.CharFilter(method='filter_by_tags')  # ?tags=apple,green (comma-separated, any match)
+    in_stock = django_filters.BooleanFilter(field_name='stock_count', lookup_expr='gt', label='In Stock Only')
 
     def filter_by_category(self, queryset, name, value):
         if value:
@@ -26,4 +27,4 @@ class ProductFilter(django_filters.FilterSet):
 
     class Meta:
         model = Product
-        fields = ['min_price', 'max_price', 'rating']
+        fields = ['min_price', 'max_price', 'min_rating']
